c224a2a7fe0abbe070bea24fec9d91d6c59a2a2d
logesh-profile <mlogeshwaran2007@gmail.com>
2026-07-04 11:20:10 +0530

    initial structure for register api

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,31 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
 
+
+@api_view(["GET"])
 def hello(request):
-    return JsonResponse({
-        "message": "hi this is from techforge backend team!"
+    return Response({
+        "success": True,
+        "message": "Welcome to TechForge , THIS IS TECHFORGE BACKEND TEAM"
     })
+
+#checks for register details validity and returns the data if valid else returns the errors
+@api_view(["POST"])
+def register(request):
+
+    serializer = RegisterSerializer(data=request.data)
+
+    if serializer.is_valid():
+
+        return Response({
+            "success": True,
+            "message": "Validation successful!",
+            "data": serializer.validated_data
+        })
+
+    return Response({
+        "success": False,
+        "errors": serializer.errors
+    }, status=400)
+
